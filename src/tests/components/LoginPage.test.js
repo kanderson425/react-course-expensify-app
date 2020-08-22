941292eb53b16ef5acb3182cbe9d5451db3b9723
kanderson425 <kyleanderson.dev@gmail.com>
2020-08-22 14:24:26 -0500

    Adds the startLogout and startLogin test cases.

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme'; 
 import { LoginPage } from '../../components/LoginPage';
 
+let startLogin, history, wrapper;
+
+beforeEach(() => {
+    startLogin = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(<LoginPage startLogin={startLogin}/>);
+  });
+
 test('should render loginPage correctly', () => {
-    const wrapper = shallow(<LoginPage />);
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLoogin on button click', () => {
+    wrapper.find('button').simulate('click');
+    expect(startLogin).toHaveBeenCalled();
 });
