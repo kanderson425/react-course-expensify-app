ee87a79083316e197b9fa29a9f24c4270225ea24
kanderson425 <kyleanderson.dev@gmail.com>
2020-08-24 08:32:49 -0500

    Modifies the firebase rules.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
     <div>
         <Switch>
-            <Route path="/" component={LoginPage} exact={true} />
+            <PublicRoute path="/" component={LoginPage} exact={true} />
             <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true} />
             <PrivateRoute path="/create" component={AddExpensePage} exact={true} />
             <PrivateRoute path="/edit/:id" component={EditExpensePage} exact={true} />
-            <Route path="/help" component={HelpPage} exact={true} />
             <Route component={NotFoundPage} />
         </Switch>
     </div>
