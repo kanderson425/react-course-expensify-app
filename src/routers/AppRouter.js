bcdd5898217ebe190b1dcf3f19368fff940fba54
kanderson425 <kyleanderson.dev@gmail.com>
2020-08-24 08:07:22 -0500

    Adds the updates for Private Routes.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,20 +6,19 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
     <Router history={history}>
     <div>
-        <Header />
         <Switch>
             <Route path="/" component={LoginPage} exact={true} />
-            <Route path="/dashboard" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} exact={true} />
-            <Route path="/edit/:id" component={EditExpensePage} exact={true} />
+            <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true} />
+            <PrivateRoute path="/create" component={AddExpensePage} exact={true} />
+            <PrivateRoute path="/edit/:id" component={EditExpensePage} exact={true} />
             <Route path="/help" component={HelpPage} exact={true} />
             <Route component={NotFoundPage} />
         </Switch>
